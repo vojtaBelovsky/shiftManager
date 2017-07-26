00eb4895d631469261c5fd19710c56e93884b19a
KrystYohoMan <labajkrystian@gmail.com>
2017-07-26 06:53:33 +0200

    TODO: textField number validation

--- ShiftManager/NewShiftView.swift
+++ ShiftManager/NewShiftView.swift
@@ -51,6 +51,28 @@ class NewShiftView: UIView {
         intervalTextField.layer.borderWidth = 1
         intervalTextField.placeholder = NSLocalizedString("CreateNewShift_loc006", comment: "")
         intervalTextField.keyboardType = UIKeyboardType.numberPad
+       /* TODO: validation numbers only not working yet
+        func textField(_ textField: UITextField,
+                       shouldChangeCharactersIn range: NSRange,
+                       replacementString string: String) -> Bool {
+            
+            // Create an `NSCharacterSet` set which includes everything *but* the digits
+            let inverseSet = NSCharacterSet(charactersIn:"0123456789").inverted
+            
+            // At every character in this "inverseSet" contained in the string,
+            // split the string up into components which exclude the characters
+            // in this inverse set
+            let components = string.components(separatedBy: inverseSet)
+            
+            // Rejoin these components
+            let filtered = components.joined(separator: "")  // use join("", components) if you are using Swift 1.2
+            
+            // If the original string is equal to the filtered string, i.e. if no
+            // inverse characters were present to be eliminated, the input is valid
+            // and the statement returns true; else it returns false
+            return string == filtered
+        }
+        */
         addSubview(intervalTextField)
         
         selectShiftColorLabel.text = ""
