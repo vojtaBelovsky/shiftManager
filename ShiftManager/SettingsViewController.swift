ed6e956309e607b4e952823c36d523c1a0ef0e1d
Tadeusz Raszka <raszka.tadeusz@gmail.com>
2017-08-15 11:59:12 +0200

    ButtonUpdate

--- ShiftManager/SettingsViewController.swift
+++ ShiftManager/SettingsViewController.swift
@@ -43,7 +43,7 @@ class SettingsViewController: UIViewController {
     }
     
     func addButtonDidPress(){
-        self.navigationController?.pushViewController(NewShiftViewController(), animated: true)
+        self.navigationController?.pushViewController(NewUserViewController(), animated: true)
     }
     
 }
