6d589c60bd5a3a1eb4620b600f01e2e48ced8f10
Tadeusz Raszka <raszka.tadeusz@gmail.com>
2017-08-16 10:54:16 +0200

    refactor HeaderView

--- ShiftManager/HeaderView.swift
+++ ShiftManager/HeaderView.swift
@@ -5,28 +5,32 @@
 //  Created by Daniel Krezelok on 10/07/2017.
 //  Copyright © 2017 Tadeusz Raszka. All rights reserved.
 //
+
 import UIKit
 import PureLayout
 
-class HeaderView: UIView {
+final class HeaderView: UIView {
     
-    let stackView = UIStackView()
-    fileprivate var dayMon = UILabel()
-    fileprivate var dayTues = UILabel()
-    fileprivate var dayWedn = UILabel()
-    fileprivate var dayThur = UILabel()
-    fileprivate var dayFri = UILabel()
-    fileprivate var daySat = UILabel()
-    fileprivate var daySun = UILabel()
+    fileprivate let stackView = UIStackView()
+    fileprivate let dayMon = UILabel()
+    fileprivate let dayTues = UILabel()
+    fileprivate let dayWedn = UILabel()
+    fileprivate let dayThur = UILabel()
+    fileprivate let dayFri = UILabel()
+    fileprivate let daySat = UILabel()
+    fileprivate let daySun = UILabel()
     
     init() {
         super.init(frame: .zero)
         stackViewInitilizer()
         setupConstraints()
     }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    func stackViewInitilizer() {
-        
+    fileprivate func stackViewInitilizer() {
         stackView.axis = .horizontal
         stackView.alignment = .center
         stackView.distribution = .equalCentering
@@ -51,16 +55,11 @@ class HeaderView: UIView {
         }
     }
     
-    func setupConstraints() {
+    fileprivate func setupConstraints() {
         stackView.autoPinEdge(toSuperviewEdge: .leading, withInset: 15)
         stackView.autoPinEdge(toSuperviewEdge: .top)
         stackView.autoMatch(.width, to: .width, of: self, withMultiplier: 0.9)
         stackView.autoPinEdge(toSuperviewEdge: .bottom)
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
 }
 
