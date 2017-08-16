44fcc804bde1058388da1344324aabaf1a9c6436
Tadeusz Raszka <raszka.tadeusz@gmail.com>
2017-08-16 10:24:57 +0200

    refactor NewUserViewController

--- ShiftManager/NewUserViewController.swift
+++ ShiftManager/NewUserViewController.swift
@@ -7,61 +7,43 @@
 //
 
 import UIKit
+
 protocol NewUserViewControllerDelegate {
     func newUserViewController(_ controller: NewUserViewController, didRegisterUser: UserModel)
 }
 
 let newUserDidRegisterNotification = Notification.Name(rawValue:"newUserDidRegisterNotification")
 
-class NewUserViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class NewUserViewController: UIViewController {
+    
     let newUserView = NewUserView()
-    let userView = UserView()
     var delegate: NewUserViewControllerDelegate?
-
+    
     override func loadView() {
-        self.view = newUserView
-        title = NSLocalizedString("RegisterTitle_loc001", comment: "")
-        self.newUserView.registerButtonDidPress(self, action: #selector(registerButtonDidPress))
-        self.newUserView.cameraButtonDidPress(self, action: #selector(cameraButtonDidPress))
+        view = newUserView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = NSLocalizedString("RegisterTitle_loc001", comment: "")
+        
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(NewUserViewController.tapDetected))
         singleTap.numberOfTapsRequired = 1
         newUserView.selectImage.isUserInteractionEnabled = true
         newUserView.selectImage.addGestureRecognizer(singleTap)
+        newUserView.registerButtonDidPress(self, action: #selector(registerButtonDidPress))
+        newUserView.cameraButtonDidPress(self, action: #selector(cameraButtonDidPress))
     }
     
     func tapDetected() {
         selectPicture()
     }
-        
-   func selectPicture() {
+    
+    func selectPicture() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        var newImage: UIImage
-        
-        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
-            newImage = possibleImage
-        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
-            newImage = possibleImage
-        } else {
-            return
-        }
-        
-        newUserView.selectImage.image = newImage
-        
-        dismiss(animated: true)
     }
     
     func cameraButtonDidPress(){
@@ -90,14 +72,32 @@ class NewUserViewController: UIViewController, UINavigationControllerDelegate, U
             alertController.addAction(defaultAction)
             present(alertController, animated: true, completion: nil)
             return
-        }
-        else {
+        } else {
             UserManager.sharedInstance.saveUser(user: user)
             sendNotification()
             navigationController?.popViewController(animated: true)
         }
     }
-    
 }
-    
 
+extension NewUserViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var newImage: UIImage
+        
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+        
+        newUserView.selectImage.image = newImage
+        
+        dismiss(animated: true)
+    }
+}
