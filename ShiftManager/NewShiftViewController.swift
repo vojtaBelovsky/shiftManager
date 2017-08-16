efa0a8b59873e2d702882e115bc07f1ea6d3ccab
Tadeusz Raszka <raszka.tadeusz@gmail.com>
2017-08-16 11:33:59 +0200

    refactor NewShiftViewController

--- ShiftManager/NewShiftViewController.swift
+++ ShiftManager/NewShiftViewController.swift
@@ -16,18 +16,18 @@ class NewShiftViewController: UIViewController, ColorSelectViewControllerDelegat
     var newShiftColor: UIColor?
     
     override func loadView() {
-        self.view = newShiftView
+        view = newShiftView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = NSLocalizedString("CreateNewShift_loc002", comment: "")
-         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonDidPress))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonDidPress))
         edgesForExtendedLayout = UIRectEdge.bottom
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         
-        self.newShiftView.setActionForColorButton(self, action: #selector(selectShiftColorButtonDidPress))
+        newShiftView.setActionForColorButton(self, action: #selector(selectShiftColorButtonDidPress))
     }
     
     func saveButtonDidPress() {
@@ -40,15 +40,14 @@ class NewShiftViewController: UIViewController, ColorSelectViewControllerDelegat
         if let interval = Int(newShiftView.interval()) {
             shift.interval = interval
         }
-
+        
         if let validationError = ShiftModelValidator.validateShift(shift) {
             let alertController = UIAlertController(title: validationError.localizedDescription, message: validationError.localizedFailureReason, preferredStyle: .alert)
             let defaultAction = UIAlertAction(title: NSLocalizedString("NewShiftAllert_loc004", comment: ""), style: .default, handler: nil)
             alertController.addAction(defaultAction)
             present(alertController, animated: true, completion: nil)
             return
-        }
-        else {
+        } else {
             ShiftManager.sharedInstance.saveShift(shift: shift)
             sendNotification()
             navigationController?.popViewController(animated:true)
@@ -59,12 +58,12 @@ class NewShiftViewController: UIViewController, ColorSelectViewControllerDelegat
         let nc = NotificationCenter.default
         nc.post(name: myNotification, object: nil)
     }
-
+    
     func selectShiftColorButtonDidPress() {
         let colorController = ColorSelectViewController()
         colorController.delegate = self
         
-        self.navigationController?.present(colorController, animated: true, completion: nil)
+        navigationController?.present(colorController, animated: true, completion: nil)
     }
     
     func colorSelectViewController(_ controller: ColorSelectViewController, didSelectColor: UIColor) {
