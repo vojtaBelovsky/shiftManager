429eed7dbb7b0c5285ead8c79380f5e1ece6d003
Tadeusz Raszka <raszka.tadeusz@gmail.com>
2017-09-14 13:10:44 +0200

    datePicker select same date bug fix

--- ShiftManager/ShiftModelValidator.swift
+++ ShiftManager/ShiftModelValidator.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFDateHelper
 
 final class ShiftModelValidator: NSObject {
     
@@ -40,23 +41,21 @@ final class ShiftModelValidator: NSObject {
             error = NSError.init(domain: "ShiftModelDomain", code: 0, userInfo: userInfo)
             return error
         }
-        /* NEMAZAT !!!!
+
         var isFirstDateOfShiftUnique = true
         UserManager.sharedInstance.selectedUser?.shifts.forEach({ shiftModel in
-            if shiftModel.firstDateOfShift porovnat shift.firstDateOfShift {
+            if shiftModel.firstDateOfShift?.normalizedDate().compare((shift.firstDateOfShift?.normalizedDate())!) == .orderedSame {
                 isFirstDateOfShiftUnique = false
-                break
             }
         })
         if !isFirstDateOfShiftUnique {
             let userInfo: [AnyHashable : Any] = [
                 NSLocalizedDescriptionKey :  NSLocalizedString("NewShiftAllert_loc001", comment: "") ,
-                NSLocalizedFailureReasonErrorKey : NSLocalizedString("NewShiftAllert_loc006", comment: "")
+                NSLocalizedFailureReasonErrorKey : NSLocalizedString("NewShiftAllert_loc007", comment: "")
             ]
             error = NSError.init(domain: "ShiftModelDomain", code: 0, userInfo: userInfo)
             return error
         }
-         NEMAZAT !!!  */
         
         if shift.color == UIColor.white || shift.color == nil || shift.color == textFields.textFieldColorWithAlpha {
             let userInfo: [AnyHashable : Any] = [
